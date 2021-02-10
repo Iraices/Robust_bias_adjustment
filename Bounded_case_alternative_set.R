b7228f28f536de2466a01f027104666c4a927f1f
Iraices <57713575+Iraices@users.noreply.github.com>
2021-02-10 10:57:03 +0100

    Add files via upload

---
+++ Bounded_case_alternative_set.R
@@ -0,0 +1,205 @@
+setwd("C:/Ivette/Papers/Paper 3/Code")
+
+library('rjags')
+library('runjags')
+library('forestplot')
+library('gtools')
+library('Matrix')
+
+## Data 
+## n_studies <- 4  ## number of studies
+obs <- matrix(c(10,80,5,17,16,41,16,44), byrow = TRUE, nrow = 4, ncol = 2)
+sample_size <- matrix(c(201,298,40,40,122,122,172,170), byrow = TRUE, nrow = 4, ncol = 2) 
+#parameters <- c("beta", "theta", "sigma2_theta", "mu", "OR", "delta", "p")
+parameters <- c("beta", "sigma2_theta", "mu", "OR", "delta", "p")
+
+colnames(obs) = c('Control', 'Treatment')
+colnames(sample_size) = c('Control', 'Treatment')
+rownames(obs) = c('REFLEX', 'WA16291', 'DANCER', 'SERENE')
+rownames(sample_size) = c('REFLEX', 'WA16291', 'DANCER', 'SERENE')
+
+
+## Bounded case 
+
+## the inequality constraint region Ax < b with x = (x1,x2,x3) is transformed into 
+## Ax = b with x = (x1,x2,x3,x4,x5,x6,x7)  
+A <- matrix(c(-1,1,0,1,0,0,0,
+              -1,0,1,0,1,0,0,
+              1,0,0,0,0,1,0,
+              -1,0,0,0,0,0,1), nrow = 4, ncol = 7, byrow = TRUE)
+b <- c(0,0,1,-0.6)
+
+# combinations of system equations with 3 variables
+extended_number_variables <- 7
+number_of_equations <- 4
+
+comb <- combinations(extended_number_variables, number_of_equations, repeats.allowed = FALSE)
+# number of combinations
+n_comb <- dim(comb)[1]
+
+## Find the points that define the convex set. 
+solutions <- matrix(0, nrow = n_comb, ncol = extended_number_variables)
+
+for(i in 1:n_comb){
+  rank_A <- rankMatrix(A[, c(comb[i,])])[1]
+  rank_Ab <- rankMatrix(cbind(A[, c(comb[i,])],b))[1]
+  nrows <- dim(A)[1]
+  if(rank_A == rank_Ab){
+    if(rank_A == nrows){
+      solutions[i, c(comb[i,])] <- solve(A[, c(comb[i,])], b)
+    }
+    if(rank_A < nrows){
+      solutions[i, c(comb[i,])] <- 'many'
+    }
+  }
+  else{
+    solutions[i,] <- NaN
+  }
+}
+
+solutions
+solutions_revised <- solutions
+
+### In the case of many solutions, due to the values must be nonnegative
+### then the solution of the systems are [0,0,0,0,0,1, -0.6]. We are adding those solutions.
+solutions_revised[29,] <- c(0,0,0,0,0,1,-0.6)
+solutions_revised[34,] <- c(0,0,0,0,0,1,-0.6)
+solutions_revised
+
+## Now, we use the variables we are interested and remove the NaN solutions
+nrows <- dim(solutions_revised[,1:3])[1]
+n_NaN<- rep(0,nrows)
+
+for(i in 1:nrows){
+  if(is.nan(as.numeric(solutions_revised[i,1]))){
+    n_NaN[i] <- i 
+  }
+  else{
+    n_NaN[i] <- 0
+  }
+}
+
+## These are the vertices of the polyhedron. 
+## We are interested in the values inside the polyhedron
+solutions_unique <- unique(solutions_revised[-c(n_NaN),1:3])
+
+## number of vertices
+num_alphas <- dim(solutions_unique)[1]
+
+all_points <- c()
+## points in the convex set
+## \sum_{i = 1}^{nrows} alpha_i * pi  with \sum_{i = 1}^{nrows} alpha_i = 1
+##
+
+alpha_1 <- seq(0,1, by = 0.1)
+L1 <- length(alpha_1)
+
+for(i in 1:L1){
+  alpha_2 <- seq(0,1 - alpha_1[i], by = 0.1)
+  L2 <- length(alpha_2)
+  for(j in 1:L2){
+    alpha_3 <- seq(0,1 - alpha_1[i] - alpha_2[j], by = 0.1)
+    L3 <- length(alpha_3)
+    for(k in 1:L3){
+      alpha_4 <- seq(0,1 - alpha_1[i] - alpha_2[j] - alpha_3[k], by = 0.1)
+      L4 <- length(alpha_4)
+      for(m in 1:L4){
+        alpha_5 <- seq(0,1 - alpha_1[i] - alpha_2[j] - alpha_3[k] - alpha_4[m], by = 0.1)
+        L5 <- length(alpha_5)
+        for(l in 1:L5){
+          alpha_6 <- seq(0,1 - alpha_1[i] - alpha_2[j] - alpha_3[k] - alpha_4[m] - alpha_5[l], by = 0.1)
+          L6 <- length(alpha_6)
+          for(n in 1:L6){
+            alpha_7 <- seq(0,1 - alpha_1[i] - alpha_2[j] - alpha_3[k] - alpha_4[m] - alpha_5[l] - alpha_6[n], by = 0.1)
+            L7 <- length(alpha_7)  
+            for(v in 1:L7){
+              alpha_8 <- seq(0,1 - alpha_1[i] - alpha_2[j] - alpha_3[k] - alpha_4[m] - alpha_5[l] - alpha_6[n] - alpha_7[v], by = 0.1)
+              L8 <- length(alpha_8)
+              for(w in 1:L8){
+                alpha_9 <- 1 - alpha_1[i] - alpha_2[j] - alpha_3[k] - alpha_4[m] - alpha_5[l] - alpha_6[n] - alpha_7[v] - alpha_8[w]
+
+                p <- alpha_1[i]* as.numeric(solutions_unique[1,]) + alpha_2[j]* as.numeric(solutions_unique[2,]) +
+                     alpha_3[k]* as.numeric(solutions_unique[3,]) + alpha_4[m]* as.numeric(solutions_unique[4,]) +
+                     alpha_5[l]* as.numeric(solutions_unique[5,]) + alpha_6[n]* as.numeric(solutions_unique[6,]) +
+                     alpha_7[v]* as.numeric(solutions_unique[7,]) + alpha_8[w]* as.numeric(solutions_unique[8,]) +
+                     alpha_9 * as.numeric(solutions_unique[9,])
+        
+                if(p[1] >= 0.6 & p[2] > 0 & p[3] > 0){
+                  all_points <- rbind(all_points, p)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+all_points <- as.data.frame(all_points, col.rows = NULL)
+colnames(all_points) <- c('Q1','Q3','Q4')
+
+## Remove duplicate points
+my_all_points <- unique(all_points) 
+
+## q_values must be a dataframe with q (study quality) combinations
+estimate_bounds_finite_set <- function(n_studies, sample_size, obs, mu_phi, q_values, 
+                                       mu_mu = 0, sigma2_mu = 10, mu_beta = 0, sigma2_beta = 10, 
+                                       alpha = 0.01, lambda = 0.01, 
+                                       n_chains = 2, n_iter = 20000){
+  
+  num_q_values <- dim(q_values)[1]
+  
+  output <- run.jags('bias_adjusted_model_150121.txt', 
+                     data <- list(n_studies = n_studies, N = sample_size, obs = obs,
+                                  q = c(q_values[1,1], q_values[1,1], q_values[1,2], q_values[1,3]),
+                                  mu_mu = mu_mu, sigma2_mu = sigma2_mu, mu_beta = mu_beta, sigma2_beta = sigma2_beta,
+                                  alpha = alpha, lambda = lambda),
+                     monitor =  c("beta", "theta", "sigma2_theta", "mu", "OR", "delta", "p"), 
+                     n.chains = n_chains, burnin = n_iter/2, 
+                     sample = n_iter, method = 'rjags')
+  
+  min_mu <- output$summary[1]$statistics['mu', 'Mean'] 
+  max_mu <- min_mu
+  
+  argmin_q <- c(q_values[1,1], q_values[1,1], q_values[1,2], q_values[1,3])
+  argmax_q <- argmin_q
+  
+  out_min_mu <- output
+  out_max_mu <- out_min_mu
+  
+  for(i in 2:num_q_values){
+    out <- run.jags('bias_adjusted_model_150121.txt', 
+                    data <- list(n_studies = n_studies, N = sample_size, obs = obs,
+                                 q = c(q_values[i,1], q_values[i,1], q_values[i,2], q_values[i,3]),
+                                 mu_mu = mu_mu, sigma2_mu = sigma2_mu, mu_beta = mu_beta, sigma2_beta = sigma2_beta,
+                                 alpha = alpha, lambda = lambda),
+                    monitor =  c("beta", "theta", "sigma2_theta", "mu", "OR", "delta", "p"), 
+                    n.chains = n_chains, burnin = n_iter/2, 
+                    sample = n_iter, method = 'rjags')
+    
+    if(out$summary[1]$statistics['mu', 'Mean'] < min_mu){
+      min_mu <- out$summary[1]$statistics['mu', 'Mean']
+      argmin_q <- c(q_values[i,1], q_values[i,1], q_values[i,2], q_values[i,3])
+      out_min_mu <- out
+    }
+    if(out$summary[1]$statistics['mu', 'Mean'] > max_mu){
+      max_mu <- out$summary[1]$statistics['mu', 'Mean']
+      argmax_q <- c(q_values[i,1], q_values[i,1], q_values[i,2], q_values[i,3])
+      out_max_mu <- out
+    }
+    
+  }
+  
+  return(list(bounds = c(minimum = min_mu, maximum = max_mu), 
+              q = list(argmin_q = argmin_q, argmax_q = argmax_q), 
+              output_minimum = out_min_mu,
+              output_maximum = out_max_mu))
+}
+
+result <- estimate_bounds_finite_set(n_studies = 4, sample_size = sample_size, obs = obs, 
+                                     q_values = my_all_points, 
+                                     mu_mu = 0, sigma2_mu = 10, mu_beta = 0, sigma2_beta = 10, 
+                                     alpha = 0.01, lambda = 0.01, 
+                                     n_chains = 2, n_iter = 20000)
+
---
+++ Foresplot_alternative.R
@@ -0,0 +1,236 @@
+## Code for paper applying robust bias adjustment modeling in evidence synthesis.
+## Plot the meta-analysis
+
+setwd("C:/Ivette/Papers/Paper 3/Code")
+
+library('rjags')
+library('runjags')
+#library('forestplot')
+library('metafor')
+library('gtools')
+library('Matrix')
+
+## Data 
+## n_studies <- 4  ## number of studies
+obs <- matrix(c(10,80,5,17,16,41,16,44), byrow = TRUE, nrow = 4, ncol = 2)
+sample_size <- matrix(c(201,298,40,40,122,122,172,170), byrow = TRUE, nrow = 4, ncol = 2) 
+parameters <- c("beta", "theta", "sigma2_theta", "mu", "OR", "delta", "p")
+
+
+colnames(obs) = c('Control', 'Treatment')
+colnames(sample_size) = c('Control', 'Treatment')
+rownames(obs) = c('REFLEX', 'WA16291', 'DANCER', 'SERENE')
+rownames(sample_size) = c('REFLEX', 'WA16291', 'DANCER', 'SERENE')
+
+## run JAGS and summarize posterior samples
+
+## Unadjusted bias model
+
+unadjusted_bias_model <- run.jags('bias_adjusted_model_150121.txt', 
+                                  data = list(n_studies = 4,
+                                              N = sample_size,
+                                              obs = obs,
+                                              q = c(1,1,1,1),
+                                              mu_mu = 0,
+                                              sigma2_mu = 10,
+                                              mu_beta = 0,
+                                              sigma2_beta = 10,
+                                              alpha = 0.01,
+                                              lambda = 0.01),
+                                  monitor = parameters, 
+                                  n.chains = 2, burnin = 5000, 
+                                  sample = 15000, method = 'rjags')
+
+unadjusted_bias_model
+
+#############################################################
+
+##Adjusted bias model
+
+adjusted_bias_model <- run.jags('bias_adjusted_model_150121.txt', 
+                                data = list(n_studies = 4,
+                                            N = sample_size,
+                                            obs = obs,
+                                            q = c(1,1,0.5,0.5),
+                                            mu_mu = 0,
+                                            sigma2_mu = 1,
+                                            mu_beta = 0,
+                                            sigma2_beta = 100,
+                                            alpha = 1,
+                                            lambda = 1),
+                                monitor = parameters, 
+                                n.chains = 2, burnin = 5000, 
+                                sample = 15000, method = 'rjags')
+
+adjusted_bias_model     
+
+#######################################################################################
+## Forestplot
+## Log Odds ratio
+study_name = c('REFLEX', 'WA16291', 'DANCER', 'SERENE', 'Summary')
+
+
+estimated_mean_unadjusted_model = c(unadjusted_bias_model$summary$statistics['delta[1]','Mean'],
+                                    unadjusted_bias_model$summary$statistics['delta[2]','Mean'],
+                                    unadjusted_bias_model$summary$statistics['delta[3]','Mean'],
+                                    unadjusted_bias_model$summary$statistics['delta[4]','Mean'], 
+                                    unadjusted_bias_model$summary$statistics['mu','Mean'])
+
+estimated_lower_ci_unadjusted_model = c(unadjusted_bias_model$summary$quantiles['delta[1]', '2.5%'],
+                                        unadjusted_bias_model$summary$quantiles['delta[2]', '2.5%'],
+                                        unadjusted_bias_model$summary$quantiles['delta[3]', '2.5%'],
+                                        unadjusted_bias_model$summary$quantiles['delta[4]', '2.5%'],
+                                        unadjusted_bias_model$summary$quantiles['mu', '2.5%'])
+
+estimated_upper_ci_unadjusted_model = c(unadjusted_bias_model$summary$quantiles['delta[1]', '97.5%'],
+                                        unadjusted_bias_model$summary$quantiles['delta[2]', '97.5%'],
+                                        unadjusted_bias_model$summary$quantiles['delta[3]', '97.5%'],
+                                        unadjusted_bias_model$summary$quantiles['delta[4]', '97.5%'],
+                                        unadjusted_bias_model$summary$quantiles['mu', '97.5%'])
+
+#################################################################################################
+
+estimated_mean_adjusted_model = c(adjusted_bias_model$summary$statistics['delta[1]','Mean'],
+                                  adjusted_bias_model$summary$statistics['delta[2]','Mean'],
+                                  adjusted_bias_model$summary$statistics['delta[3]','Mean'],
+                                  adjusted_bias_model$summary$statistics['delta[4]','Mean'], 
+                                  adjusted_bias_model$summary$statistics['mu','Mean'])
+
+
+estimated_lower_ci_adjusted_model = c(adjusted_bias_model$summary$quantiles['delta[1]', '2.5%'],
+                                      adjusted_bias_model$summary$quantiles['delta[2]', '2.5%'],
+                                      adjusted_bias_model$summary$quantiles['delta[3]', '2.5%'],
+                                      adjusted_bias_model$summary$quantiles['delta[4]', '2.5%'],
+                                      adjusted_bias_model$summary$quantiles['mu', '2.5%'])
+
+
+estimated_upper_ci_adjusted_model = c(adjusted_bias_model$summary$quantiles['delta[1]', '97.5%'],
+                                      adjusted_bias_model$summary$quantiles['delta[2]', '97.5%'],
+                                      adjusted_bias_model$summary$quantiles['delta[3]', '97.5%'],
+                                      adjusted_bias_model$summary$quantiles['delta[4]', '97.5%'],
+                                      adjusted_bias_model$summary$quantiles['mu', '97.5%'])
+
+
+## Forestplot of unadjusted model
+forest(x = estimated_mean_unadjusted_model, 
+       ci.lb = estimated_lower_ci_unadjusted_model, 
+       ci.ub = estimated_upper_ci_unadjusted_model,
+       xlab = 'Odds Ratio',
+       slab = study_name, 
+       pch = c(rep(15,4),18), 
+       col = c(rep("black",4),"red"),
+       header = 'Study',ylim = c(0,13),
+       rows = c(10,8,6,4,2))
+
+######################################################## 
+## Forestplot of unadjusted model
+forest(x = estimated_mean_unadjusted_model[1:4], 
+       ci.lb = estimated_lower_ci_unadjusted_model[1:4], 
+       ci.ub = estimated_upper_ci_unadjusted_model[1:4],
+       xlab = 'Log-Odds Ratio',
+       slab = study_name[1:4], 
+       pch = rep(15,4), 
+       col = rep("black",4),
+       header = 'Study', ylim = c(0,13),
+       rows = c(10,8,6,4))
+
+## Adding the summary results
+addpoly(x = estimated_mean_unadjusted_model[5], 
+        ci.lb = estimated_lower_ci_unadjusted_model[5], 
+        ci.ub = estimated_upper_ci_unadjusted_model[5], 
+        col = 'black', border = 'black', rows = 2, annotate = TRUE, cex = 1, 
+        mlab = "Summary")
+
+## Result of RBA
+#load('RBA_result_9points_alphas_by_01.Rdata')
+load('RBA_result_9points_alphas_by_01_new.Rdata')
+
+RBA_lower_mean_adjusted_model = c(result$output_minimum$summary[1]$statistics['delta[1]', 'Mean'],
+                                  result$output_minimum$summary[1]$statistics['delta[2]', 'Mean'],
+                                  result$output_minimum$summary[1]$statistics['delta[3]', 'Mean'],
+                                  result$output_minimum$summary[1]$statistics['delta[4]', 'Mean'],
+                                  result$output_minimum$summary[1]$statistics['mu', 'Mean'])
+
+RBA_upper_mean_adjusted_model = c(result$output_maximum$summary[1]$statistics['delta[1]', 'Mean'],
+                                  result$output_maximum$summary[1]$statistics['delta[2]', 'Mean'],
+                                  result$output_maximum$summary[1]$statistics['delta[3]', 'Mean'],
+                                  result$output_maximum$summary[1]$statistics['delta[4]', 'Mean'],
+                                  result$output_maximum$summary[1]$statistics['mu', 'Mean'])
+
+RBA_lower_ci_adjusted_model = c(result$output_minimum$summary$quantiles['delta[1]', '2.5%'],
+                                result$output_minimum$summary$quantiles['delta[2]', '2.5%'],
+                                result$output_minimum$summary$quantiles['delta[3]', '2.5%'],
+                                result$output_minimum$summary$quantiles['delta[4]', '2.5%'],
+                                result$output_minimum$summary$quantiles['mu', '2.5%'])
+
+RBA_upper_ci_adjusted_model = c(result$output_maximum$summary$quantiles['delta[1]', '97.5%'],
+                                result$output_maximum$summary$quantiles['delta[2]', '97.5%'],
+                                result$output_maximum$summary$quantiles['delta[3]', '97.5%'],
+                                result$output_maximum$summary$quantiles['delta[4]', '97.5%'],
+                                result$output_maximum$summary$quantiles['mu', '97.5%'])
+
+## Adding the results of RBA to the foresplot
+# Study 1 REFLEX
+polygon(x = c(RBA_lower_mean_adjusted_model[1], RBA_upper_mean_adjusted_model[1],RBA_upper_mean_adjusted_model[1], 
+              RBA_lower_mean_adjusted_model[1]), c(8.9,8.9,9.1,9.1), col = 'blue', border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[1], RBA_upper_ci_adjusted_model[1]), c(9,9), border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[1], RBA_lower_ci_adjusted_model[1]), c(8.9,9.1), border = 'blue')
+polygon(x = c(RBA_upper_ci_adjusted_model[1], RBA_upper_ci_adjusted_model[1]), c(8.9,9.1), border = 'blue')
+text(3, 9, labels = paste0('(', min(round(RBA_lower_mean_adjusted_model[1], 2),
+                                    round(RBA_upper_mean_adjusted_model[1],2)),'--',
+                                max(round(RBA_lower_mean_adjusted_model[1], 2),
+                                    round(RBA_upper_mean_adjusted_model[1],2)),')', ' ',
+                            '[',round(RBA_lower_ci_adjusted_model[1], 2),', ',
+                                round(RBA_upper_ci_adjusted_model[1],2),']'), col = "blue", pos=4, cex=1)
+
+# Study 2 WA16291
+polygon(x = c(RBA_lower_mean_adjusted_model[2], RBA_upper_mean_adjusted_model[2],RBA_upper_mean_adjusted_model[2], 
+              RBA_lower_mean_adjusted_model[2]), c(6.9,6.9,7.1,7.1), col = 'blue', border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[2], RBA_upper_ci_adjusted_model[2]), c(7,7), border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[2], RBA_lower_ci_adjusted_model[2]), c(6.9,7.1), border = 'blue')
+polygon(x = c(RBA_upper_ci_adjusted_model[2], RBA_upper_ci_adjusted_model[2]), c(6.9,7.1), border = 'blue')
+text(3, 7, labels = paste0('(', min(round(RBA_lower_mean_adjusted_model[2], 2),
+                                    round(RBA_upper_mean_adjusted_model[2],2)),'--',
+                           max(round(RBA_lower_mean_adjusted_model[2], 2),
+                               round(RBA_upper_mean_adjusted_model[2],2)), ')', ' ',
+                           '[',round(RBA_lower_ci_adjusted_model[2], 2),', ',
+                           round(RBA_upper_ci_adjusted_model[2],2),']'), col = "blue", pos=4, cex=1)
+
+
+# Study 3 DANCER 
+polygon(x = c(RBA_lower_mean_adjusted_model[3], RBA_upper_mean_adjusted_model[3],
+              RBA_upper_mean_adjusted_model[3], RBA_lower_mean_adjusted_model[3]), c(4.9,4.9,5.1,5.1), col = 'blue', border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[3], RBA_upper_ci_adjusted_model[3]), c(5,5), border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[3], RBA_lower_ci_adjusted_model[3]), c(4.9,5.1), border = 'blue')
+polygon(x = c(RBA_upper_ci_adjusted_model[3], RBA_upper_ci_adjusted_model[3]), c(4.9,5.1), border = 'blue')
+text(3, 5, labels = paste0('(',min(round(RBA_lower_mean_adjusted_model[3], 2),
+                                   round(RBA_upper_mean_adjusted_model[3],2)),'--',
+                           max(round(RBA_lower_mean_adjusted_model[3], 2),
+                               round(RBA_upper_mean_adjusted_model[3],2)), ')', ' ',
+                           '[',round(RBA_lower_ci_adjusted_model[3], 2),', ',
+                           round(RBA_upper_ci_adjusted_model[3],2),'0',']'), col = "blue", pos=4, cex=1)
+
+
+#Study 4 SERENE
+polygon(x = c(RBA_lower_mean_adjusted_model[4], RBA_upper_mean_adjusted_model[4],
+              RBA_upper_mean_adjusted_model[4], RBA_lower_mean_adjusted_model[4]), c(2.9,2.9,3.1,3.1), col = 'blue', border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[4], RBA_upper_ci_adjusted_model[4]), c(3,3), border = 'blue')
+polygon(x = c(RBA_lower_ci_adjusted_model[4], RBA_lower_ci_adjusted_model[4]), c(2.9,3.1), border = 'blue')
+polygon(x = c(RBA_upper_ci_adjusted_model[4], RBA_upper_ci_adjusted_model[4]), c(2.9,3.1), border = 'blue')
+text(3, 3, labels = paste0('(',min(round(RBA_lower_mean_adjusted_model[4], 2),
+                                   round(RBA_upper_mean_adjusted_model[4],2)),'--',
+                               max(round(RBA_lower_mean_adjusted_model[4], 2),
+                               round(RBA_upper_mean_adjusted_model[4],2)), ')', ' ',
+                           '[',round(RBA_lower_ci_adjusted_model[4], 2),', ',
+                           round(RBA_upper_ci_adjusted_model[4],2),']'), col = "blue", pos=4, cex=1)
+
+
+##Summary
+polygon(x = c(RBA_lower_ci_adjusted_model[5], RBA_lower_mean_adjusted_model[5], 
+              RBA_upper_mean_adjusted_model[5], RBA_upper_ci_adjusted_model[5],
+              RBA_upper_mean_adjusted_model[5], RBA_lower_mean_adjusted_model[5],
+              RBA_lower_ci_adjusted_model[5]), c(1,1.5,1.5,1,0.5,0.5,1), col = 'blue', border = 'blue')
+text(3, 1, labels = paste0('(',round(RBA_lower_mean_adjusted_model[5], 2),'--',
+                             round(RBA_upper_mean_adjusted_model[5],2), ')', ' ',
+                             '[',round(RBA_lower_ci_adjusted_model[5], 2),', ',
+                             round(RBA_upper_ci_adjusted_model[5],2),']'), col = "blue", pos=4, cex=1)
